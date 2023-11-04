a910a1634edb7302da835baec55460f33766b4e9
NithinYesudas <nithinypadickal@gmail.com>
2023-11-04 13:01:25 +0530

    Added basic model classes

---
+++ lib/Model/Seeker.dart
@@ -0,0 +1,11 @@
+class Seeker{
+  String name;
+  String email;
+  List<String> applications;
+  Seeker({
+    required this.name,
+    required this.email,
+    required this.applications
+});
+
+}
---
+++ lib/View/LandingPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(child: Image.asset('assets/images/img.png'),)
+        ],
+      ),
+      
+    );
+  }
+}
